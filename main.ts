f2ecf1d15e62a4b62d33119fe6128af0a68afa17
viaanpravin866-blip <viaanpravin866@gmail.com>
2026-01-21 21:04:13 +0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+music.play(music.stringPlayable("C G F D C5 - C5 F ", 120), music.PlaybackMode.LoopingInBackground)
+basic.showIcon(IconNames.SmallHeart)
+basic.showLeds(`
+    . . # . .
+    # # # # #
+    . . # . .
+    . . # . .
+    . . # . .
+    `)
 basic.forever(function () {
 	
 })
